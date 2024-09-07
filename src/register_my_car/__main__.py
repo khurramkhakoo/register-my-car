bc422996f51a25183fdf3652e82e83bac6e51986
khurramkhakoo <khurramkhakoo@gmail.com>
2024-09-07 17:53:44 -0500

    adding headless

--- src/register_my_car/__main__.py
+++ src/register_my_car/__main__.py
@@ -1,16 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import time
 
 from constants import URL
 from util import *
 
 # Path to your ChromeDriver
-chrome_driver_path = '/usr/local/bin/chromedriver'
+chrome_driver_path = '/usr/bin/chromedriver'
+service:Service = Service(chrome_driver_path)
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
 
 # Initialize WebDriver
-service:Service = Service(chrome_driver_path)
-driver:webdriver = webdriver.Chrome(service = service)
+driver:webdriver = webdriver.Chrome(service=service, options=options)
 
 # Navigate to the website
 driver.get(URL)
